388498bf54ce53be77dc1a25ddd4d67f1fe01052
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2021-04-28 22:32:28 -0500

    se modifican los tipos y el num de valores que retorna la funcion de escaneo

--- scripts/module_scan.py
+++ scripts/module_scan.py
@@ -159,4 +159,5 @@ def scan_by_interface(interface_name="tap0",user="admin",password="admin",secret
     # Posición 0 devuelve el json de todas las interfaces acomadado
     # Posición 1 devuelve el arreglo de interconexiones que hay entre routers
     # Posicion 2 devuelve todos los host que responsidieron al ping
-    return [json_conexiones,arr_conexiones,json_respond]
+    return [conexiones_r, arr_conexiones, net_router, responde]
+    #return [json_conexiones,arr_conexiones,json_respond]
